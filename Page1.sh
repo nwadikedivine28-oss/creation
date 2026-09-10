17e545ca1c518c542acf3b6e1e15efbc9b55b99f
DangoDivs <nwadikedivine28@gmail.com>
2026-09-10 18:24:08 +0100

    update Page1.sh

--- Page1.sh
+++ Page1.sh
@@ -1,2 +1,7 @@
-read -p "Who are you " you
-echo "you"
+read -p "Who are human" you
+if [ "$you" = "Yes" ] || [ "$you" = "yes" ]; then
+echo "good then"
+elif [ "$you" = "No"] || [ "$you" = "no" ]; then
+echo "opps you are not welcome"
+read -p "Are you a graduate"
+if [ "$Answer" = "Yes" ] || [ "$Answer" = "yes" ]
