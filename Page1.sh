222d33f47a11155084279c8c26b9da861401d0ec
DangoDivs <nwadikedivine28@gmail.com>
2026-09-10 21:36:20 +0100

    update Page1.sh

--- Page1.sh
+++ Page1.sh
@@ -4,9 +4,11 @@ echo "good then"
 elif [ "$you" = "No"] || [ "$you" = "no" ]; then
 echo "opps you are not welcome"
 else "i don't know who you are"
+fi
 read -p "Are you a graduate" me
 if [ "$me" = "Yes" ] || [ "$me" = "yes" ]; then
 echo "good then"
 elif [ "$me" = "No"] || [ "$me" = "no" ]; then
 echo "opps you are not a graduate"
 else "can't validate"
+fi
