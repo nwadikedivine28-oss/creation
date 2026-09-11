ca2e02287d57f3049f951173ed6789b6c92af1d5
DangoDivs <nwadikedivine28@gmail.com>
2026-09-11 17:12:32 +0100

    update Page1.sh

--- Page1.sh
+++ Page1.sh
@@ -1,14 +1,14 @@
-read -p "Who are human answer either yes or no" you
+read -p "are you human?, answer either yes or no" you
 if [ "$you" = "Yes" ] || [ "$you" = "yes" ]; then
 echo "good then"
 elif [ "$you" = "No"] || [ "$you" = "no" ]; then
 echo "opps you are not welcome"
-else "i don't know who you are"
+else  echo "i don't know who you are"
 fi
 read -p "Are you a graduate" me
 if [ "$me" = "Yes" ] || [ "$me" = "yes" ]; then
 echo "good then"
 elif [ "$me" = "No"] || [ "$me" = "no" ]; then
 echo "opps you are not a graduate"
-else "can't validate"
+else echo "can't validate"
 fi
